8948ea9a52c3facf0d8fe226b4c2f5cf1f9a4f1b
Sammy Hasan <sammy.nm.hasan@gmail.com>
2017-09-19 15:51:55 +0100

    universal stocj chooser
    removed comment

--- stock_chooser.cpp
+++ stock_chooser.cpp
@@ -23,7 +23,6 @@ int main(void){
   cout << "Enter number of choices to make as Int: ";
   cin >> num_choices;
 
-  // int arr_stocks[] = new int[num_stocks];
   vector<int> vec_stocks(num_stocks);
   iota(begin(vec_stocks), end(vec_stocks), 0);
 
